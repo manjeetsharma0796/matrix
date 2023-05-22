3673c49890996a0ca395aa89816b274c9c81114f
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-22 21:50:27 +0530

    Added Matrix class which has cycler and animation

--- matrix.js
+++ matrix.js
@@ -1,43 +1,34 @@
-const advance = (list) => {
-  let temp = list.pop();
-  list.unshift(temp);
+class Matrix {
+  #matrix
 
-  return list;
-};
+  constructor(grid) {
+    this.#matrix = grid;
+  };
 
-const impede = (list) => {
-  let temp = list.shift();
-  list.push(temp);
+  #cycler() {
+    const lastRow = this.#matrix.pop();
+    this.#matrix.unshift(lastRow);
+  }
 
-  return list;
+  animate() {
+    this.#cycler();
+    return this.#matrix.map((element) => element.join('  ')).join('\n');
+  }
 }
 
-const matrix = () => {
-  let space = new Array(9).fill(' ');
-  let space1 = new Array(9).fill(' ');
-  // const grid = new Array(2).fill().map(() => new Array(2).fill(1));
+const grid = [
+  [1, 'a', 'x', 'ぁ'],
+  [2, 'b', 'あ', 'y'],
+  [3, 'c', 'z', 'ぃ'],
+  [2, 'b', 'い', 'y'],
+  [1, 'a', 'x', 'ぇ'],
+  [2, 'b', 'ぉ', 'y'],
+  [3, 'c', 'z', 'き']
+];
 
-  space[0] = 'せ';
-  space[1] = 'ぁ';
-  space[2] = 'ぉ';
+const p = new Matrix(grid);
 
-  space1[6] = 'せ';
-  space1[7] = 'ぁ';
-  space1[8] = 'ぉ';
-  // grid[0][1] = 'm';
-  // console.log(grid.join('\n'));
-  // grid.forEach((line) => {
-  //   console.log(line[0])
-  // })
-
-  setInterval(() => {
-    console.clear();
-    console.log('\033[38;5;82m' + [space.join('\n'),space1.join('')]);
-
-    space = advance(space);
-    space1 = impede(space1);
-
-  }, 300);
-};
-
-matrix();
+setInterval(() => {
+  console.clear();
+  console.log('\033[38;5;82m' + p.animate());
+}, 300);
